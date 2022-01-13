9625b2c50335af517a42075844ee86803bf8ad38
atharva21-stack <tweenmotion21@gmail.com>
2022-01-12 16:09:52 -0800

    Added assets folder
    Signed-off-by: atharva21-stack <tweenmotion21@gmail.com>

---
+++ assets/js/revolution/revolution-init.js
@@ -0,0 +1,69 @@
+var tpj = jQuery;
+
+var revapi4;
+tpj(document).ready(function () {
+  if (tpj("#rev_slider_4_1").revolution == undefined) {
+    revslider_showDoubleJqueryError("#rev_slider_4_1");
+  } else {
+    revapi4 = tpj("#rev_slider_4_1").show().revolution({
+      sliderType: "standard",
+      jsFileLocation: "revolution/js/",
+      sliderLayout: "auto",
+      dottedOverlay: "none",
+      delay: 5000,
+      navigation: {
+        keyboardNavigation: "off",
+        keyboard_direction: "horizontal",
+        mouseScrollNavigation: "off",
+        mouseScrollReverse: "default",
+        onHoverStop: "off",
+        touch: {
+          touchenabled: "on",
+          swipe_threshold: 75,
+          swipe_min_touches: 1,
+          swipe_direction: "horizontal",
+          drag_block_vertical: false
+        },
+        arrows: {
+          enable: true,
+          style: 'uranus'
+        }
+      },
+      responsiveLevels: [1920, 1370, 770, 490],
+      visibilityLevels: [1920, 1370, 770, 490],
+      gridwidth: [1170, 1010, 730, 460],
+      gridheight: [970, 670, 600, 500],
+      lazyType: "none",
+      parallax: {
+        type: "mouse",
+        origo: "slidercenter",
+        speed: 2000,
+        levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50, 46, 47, 48, 49, 50, 55],
+        type: "mouse",
+        disable_onmobile: "on"
+      },
+      shadow: 0,
+      spinner: "off",
+      stopLoop: "off",
+      stopAfterLoops: -1,
+      stopAtSlide: 1,
+      shuffle: "off",
+      autoHeight: "off",
+      fullScreenAutoWidth: "off",
+      fullScreenAlignForce: "off",
+      fullScreenOffsetContainer: ".header",
+      fullScreenOffset: "60px",
+      disableProgressBar: "on",
+      hideThumbsOnMobile: "off",
+      hideSliderAtLimit: 0,
+      hideCaptionAtLimit: 0,
+      hideAllCaptionAtLilmit: 0,
+      debugMode: false,
+      fallbacks: {
+        simplifyAll: "off",
+        nextSlideOnWindowFocus: "off",
+        disableFocusListener: false,
+      }
+    });
+  }
+});
